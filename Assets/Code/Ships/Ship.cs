218f2ffc13f4243068d73a5c6ad1feef847a7991
MrTraner <juanmauricio.ochoa@hotmail.com>
2023-05-31 00:28:00 -0600

    Movimiento de la nave
    - Versión inicial del movimiento de la nave
    - Sprite inicial de la nave
    - Plugin de Joystick

---
+++ Assets/Code/Ships/Ship.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ships
+{
+    public class Ship : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private Joystick _joystick;
+        [SerializeField] private bool useJoystick;
+        private Transform _myTransform;
+        private Camera _camera;
+
+        void Awake()
+        {
+            _myTransform = transform;
+            _camera = Camera.main;
+        }
+
+        void Update()
+        {
+            var direction = GetDirection();
+            Move(direction);
+        }
+
+        private Vector2 GetDirection()
+        {
+            if (useJoystick)
+            {
+                return new Vector2(_joystick.Horizontal, _joystick.Vertical);
+            }
+            else
+            {
+                var horizontalDir = Input.GetAxis("Horizontal");
+                var verticalDir = Input.GetAxis("Vertical");
+
+                return new Vector2(horizontalDir, verticalDir);
+            }
+        }
+
+        private void Move(Vector2 direction)
+        {
+            _myTransform.Translate(direction * (_speed * Time.deltaTime));
+            
+            ClampFinalPosition();
+        }
+
+        private void ClampFinalPosition()
+        {
+            var viewportPoint = _camera.WorldToViewportPoint(_myTransform.position);
+
+            float minValue = 0.03f;
+            float maxValue = 0.97f;
+
+            viewportPoint.x = Mathf.Clamp(viewportPoint.x, minValue, maxValue);
+            viewportPoint.y = Mathf.Clamp(viewportPoint.y, minValue, maxValue);
+
+            _myTransform.position = _camera.ViewportToWorldPoint(viewportPoint);
+        }
+    }
+}
