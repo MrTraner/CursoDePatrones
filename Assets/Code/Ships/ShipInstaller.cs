a83eac8d51c466e701d63caeea8463ec4faead53
MrTraner <juanmauricio.ochoa@hotmail.com>
2023-06-01 22:38:36 -0600

    IA Simple
    Implementación de una IA simple para controlar la nave usando el patrón adapter y strategy

--- Assets/Code/Ships/ShipInstaller.cs
+++ Assets/Code/Ships/ShipInstaller.cs
@@ -6,6 +6,7 @@ namespace Ships
 {
     public class ShipInstaller : MonoBehaviour
     {
+        [SerializeField] private bool _useAI;
         [SerializeField] private bool _useJoystick;
         [SerializeField] private Joystick _joystick;
         [SerializeField] private Ship _ship;
@@ -17,15 +18,14 @@ namespace Ships
 
         private IInput GetInput()
         {
+            if (_useAI)
+                return new AIInputAdapter(_ship);
+
             if (_useJoystick)
-            {
                 return new JoystickInputAdapter(_joystick);
-            }
-            else
-            {
-                Destroy(_joystick.gameObject);
-                return new UnityInputAdapter();
-            }
+            
+            Destroy(_joystick.gameObject);
+            return new UnityInputAdapter();
         }
     }
 }
