56f08b077e018abe6bfb0e586bc0cef158d58cb0
MrTraner <juanmauricio.ochoa@hotmail.com>
2023-06-02 21:09:40 -0600

    Actualización Ship
    Se elimina código comentado que no se está utilizando

--- Assets/Code/Ships/Ship.cs
+++ Assets/Code/Ships/Ship.cs
@@ -7,11 +7,8 @@ namespace Ships
     public class Ship : MonoBehaviour
     {
         [SerializeField] private float _speed;
-        //[SerializeField] private Joystick _joystick;
         private IInput _input;
-        //[SerializeField] private bool useJoystick;
         private Transform _myTransform;
-        //private Camera _camera;
         private ICheckLimits _checkLimits;
 
         public void Configure(IInput input, ICheckLimits checkLimits)
@@ -23,7 +20,6 @@ namespace Ships
         void Awake()
         {
             _myTransform = transform;
-            //_camera = Camera.main;
         }
 
         void Update()
@@ -34,17 +30,6 @@ namespace Ships
 
         private Vector2 GetDirection()
         {
-            /*if (useJoystick)
-            {
-                return new Vector2(_joystick.Horizontal, _joystick.Vertical);
-            }
-            else
-            {
-                var horizontalDir = Input.GetAxis("Horizontal");
-                var verticalDir = Input.GetAxis("Vertical");
-
-                return new Vector2(horizontalDir, verticalDir);
-            }*/
             return _input.GetDirection();
         }
 
@@ -52,21 +37,7 @@ namespace Ships
         {
             _myTransform.Translate(direction * (_speed * Time.deltaTime));
 
-            //ClampFinalPosition();
             _checkLimits.ClampFinalPosition();
         }
-
-        /*private void ClampFinalPosition()
-        {
-            var viewportPoint = _camera.WorldToViewportPoint(_myTransform.position);
-
-            float minValue = 0.03f;
-            float maxValue = 0.97f;
-
-            viewportPoint.x = Mathf.Clamp(viewportPoint.x, minValue, maxValue);
-            viewportPoint.y = Mathf.Clamp(viewportPoint.y, minValue, maxValue);
-
-            _myTransform.position = _camera.ViewportToWorldPoint(viewportPoint);
-        }*/
     }
 }
